92c5ed57a420838d0b9caed5acce90d105601f7b
MarcelToebben <marcel.toebben594@gmail.com>
2025-09-07 23:58:01 +0200

    feat: start project

---
+++ js/game.js
@@ -0,0 +1,7 @@
+let canvas;
+let charakter = new Image();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    charakter.src = '../img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png'
+}
